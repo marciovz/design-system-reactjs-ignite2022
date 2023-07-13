b75f8f2846a149b5d939ed99365ff4b782d88a9d
marciovz <marciovz@yahoo.com.br>
2023-07-13 16:55:59 -0300

    Configuração tsconfig e do  tema dark

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,4 +1,7 @@
+import { themes } from '@storybook/theming'
+
 /** @type { import('@storybook/react').Preview } */
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,6 +11,9 @@ const preview = {
         date: /Date$/,
       },
     },
+    docs: {
+      theme: themes.dark
+    }
   },
 };
 
