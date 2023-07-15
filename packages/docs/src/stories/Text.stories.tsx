75c2738bf39ddf018a8dde995738a96562948ed4
marciovz <marciovz@yahoo.com.br>
2023-07-15 20:48:21 -0300

    Atualizado componentes Box e Text

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -4,6 +4,7 @@ import { Text, TextProps } from '@ignite-ui/react'
 export default {
   title: 'Typography/Text',
   component: Text,
+  tags: ['autodocs'],
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam non, iure a voluptate voluptatibus sint, dolorem eveniet mollitia hic qui neque officia architecto quibusdam sequi eos laboriosam provident distinctio atque?',
